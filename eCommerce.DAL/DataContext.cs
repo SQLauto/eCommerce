7ce7ed29a75aaddd61b09776b573270d0427e7ac
Carl Blance <carl.blance@live.com>
2015-11-24 04:30:12 +1300

    Data Context has been created
    Controllers can now access the data context, but will access it using
    repostories.

---
+++ eCommerce.DAL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using eCommerce.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.DAL.Data
+{
+    public class DataContext : DbContext
+    {
+        //Default database connection
+        public DataContext()
+            : base("DefaultConnection") {}
+
+        //Entities are declared here
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
--- eCommerce.WebUI/Controllers/HomeController.cs
+++ eCommerce.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eCommerce.DAL.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,9 @@ namespace eCommerce.WebUI.Controllers
     {
         public ActionResult Index()
         {
+            /* Used to test to see if access is given
+            DataContext context = new DataContext();
+             */ 
             return View();
         }
 
