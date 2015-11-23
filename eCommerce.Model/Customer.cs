43dd15486043b9863b235ed11f0d53ae32849661
Carl Blance <carl.blance@live.com>
2015-11-24 04:15:35 +1300

    Basic Models have been created
    Models have been populated to be used by the database later on

---
+++ eCommerce.Model/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Model
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+
+        public string Address1 { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+
+        //Add First Name, Last Name, Suburb, Phone, Mobile once basic functionailty has been created
+    }
+}
